9d44b783a0b6a03b83d71ff3c04bdd87718fe697
fishdmy <122056731+fishdmy@users.noreply.github.com>
2023-03-13 13:23:51 -0700

    Add files via upload

--- scripts/streamlit_app.py
+++ scripts/streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import folium
 import streamlit_folium
 
-app_title = "Narcan Deployment in San Francisco, 2018-2023"
+app_title = "Drug-related arrests in San Francisco, 2018-2023"
 app_subtitle = "Source: Data SF / SFPD"
 
 def main():
@@ -12,27 +12,30 @@ def main():
     st.caption(app_subtitle)
 
     ## load data
-    df = pd.read_csv("/Users/mengyu/code/sfpd-data/data/processed/narcan_deployment.csv")
+    df = pd.read_csv("/Users/mengyu/code/sfpd-data/data/processed/drug_arrest_data.csv")
     field_name = "Incident ID"
-    metric_title = "Total number of Narcan deployment:"
+    metric_title = "Total number of drug-related arrests:"
     # total = df[field_name].count()
     # st.metric(metric_title, total)
     df = df.drop(columns=["Unnamed: 0"])
 
     ## display filters and map
     year_options = ['All years'] + sorted(df['Incident Year'].unique(), reverse=True)
-    district_options = ['All police districts'] + sorted(df['Police District'].unique())
+    neighborhood_options = ['All neighborhoods'] + sorted(df['Analysis Neighborhood'].astype(str).unique())
 
     year_filter = st.selectbox('Select year:', year_options)
-    district_filter = st.selectbox('Select police district:', district_options)
+    neighborhood_filter = st.selectbox('Select neighborhod:', neighborhood_options)
 
     if year_filter == 'All years':
         filtered_df = df
     else:
         filtered_df = df[df['Incident Year'] == year_filter]
 
-    if district_filter != 'All police districts':
-        filtered_df = filtered_df[filtered_df['Police District'] == district_filter]
+    if neighborhood_filter != 'All neighborhoods':
+        filtered_df = filtered_df[filtered_df['Analysis Neighborhood'] == neighborhood_filter]
+
+    # Remove any rows with missing latitude or longitude values
+    filtered_df = filtered_df.dropna(subset=['Latitude', 'Longitude'])
 
     # update the total based on the filters
     total = filtered_df[field_name].count()
@@ -46,7 +49,10 @@ def main():
     m = folium.Map(location=map_center, zoom_start=12)
     for index, row in filtered_df.iterrows():
         tooltip = f"Incident Datetime: {row['Incident Datetime']}<br>Intersection: {row['Intersection']}"
-        folium.Marker([row['Latitude'], row['Longitude']], tooltip=tooltip).add_to(m)
+        folium.Marker(
+            [row['Latitude'], row['Longitude']], 
+        tooltip=tooltip
+        ).add_to(m)
 
     # Display the filtered map using streamlit_folium
     st.write(streamlit_folium.folium_static(m))
